d4fa2b740b8ff566d1858772b7b388f6f76bc1ea
raiti-chan <rusuke@t-net.ne.jp>
2022-04-22 19:29:06 +0900

    ファイル名にできない文字の置き換えようメソッドの追加

--- Assets/Raitichan/Script/Util/Editor/AssetPathUtil.cs
+++ Assets/Raitichan/Script/Util/Editor/AssetPathUtil.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Raitichan.Script.Util.Editor {
 	public static class AssetPathUtil {
+
+		private static readonly char[] _INVALID_CHARS = Path.GetInvalidPathChars();
 
 		public static string GetAssetsPath(string fullPath) {
 			int startIndex = fullPath.IndexOf("Assets/", StringComparison.Ordinal);
@@ -16,6 +19,16 @@ namespace Raitichan.Script.Util.Editor {
 
 		public static string GetFullPath(string assetPath) {
 			return Path.GetFullPath(assetPath);
+		}
+		
+		/// <summary>
+		/// ファイル名から無効な文字を指定した文字に置き換えます。
+		/// </summary>
+		/// <param name="fileName">ファイル名</param>
+		/// <param name="replace">置き換える文字</param>
+		/// <returns>置き換えられたファイル名</returns>
+		public static string ReplaceInvalidFileNameChars(string fileName, char replace) {
+			return string.Concat(fileName.Select(c => _INVALID_CHARS.Contains(c) ? replace : c));
 		}
 	}
 }
